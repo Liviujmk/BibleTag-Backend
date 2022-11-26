f4aaa4ef1c25bb866a0354576ce4f5c483230546
Liviu Mitrofan <64259428+Liviujmk@users.noreply.github.com>
2022-11-26 21:34:46 +0200

    Update article.js

--- models/article.js
+++ models/article.js
@@ -5,7 +5,7 @@ const ArticleSchema = new mongoose.Schema({
     title: String,
     content: String,
     createdAt: { type: Date, default: Date.now },
-    collectionName: [{type: String}],
+    collectionName: String,
     collectionId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Collection'
